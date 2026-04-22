a34aff59e61b37265974e779832e7d608dd9bd74
Mattias Kockum <mattias@kockum.net>
2026-04-23 01:03:19 +0200

    refactor: move toplevelRegistry into separate module
    Move 'terminals' and 'toplevelRegistry' into a new 'registry' directory.

--- default.nix
+++ default.nix
@@ -14,14 +14,11 @@
 }:
 let
   inherit (lib)
-    concatStringsSep
     filter
-    attrNames
     mapAttrs'
     isFunction
     nameValuePair
     removeSuffix
-    mapAttrs
     ;
 
   defaultEdition = "acmecorp-bureautix";
@@ -137,36 +134,11 @@ let
   };
 
   installers = import ./installers moduleArgs;
+  registry = import ./registry moduleArgs;
 in
 rec {
   inherit (installers) net-installer usb-installer;
-
-  # { <serial number1>, <serial number2>, ... }
-  terminals = mapAttrs (
-    serial:
-    { machineModule, userModules }:
-    securix.lib.mkTerminal {
-      name = serial;
-      userSpecificModule = { };
-      vpnProfiles = { };
-      modules = [
-        machineModule
-        ./common
-      ]
-      ++ userModules;
-    }
-  ) (securix.lib.readInventory2 { dir = ./inventory; });
-
-  # Toplevel registry:
-  # iterate over all terminals and perform: $serial  $toplevel generation.
-  # This builds ALL system configurations.
-  toplevelRegistry =
-    let
-      toplevels = map (serial: "${serial} ${terminals.${serial}.system.config.system.build.toplevel}") (
-        attrNames terminals
-      );
-    in
-    pkgs.writeText "toplevels" (concatStringsSep "\n" toplevels);
+  inherit (registry) terminals toplevelRegistry;
 
   shell = pkgs.mkShell {
     # Inspired by DGNum's infrastructure.
